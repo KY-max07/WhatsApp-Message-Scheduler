87ede2f442a5909b06949c13fad55789642468ff
Koushik Yerraguntla <koushikyerraguntla@gmail.com>
2025-03-14 02:47:08 +0530

    Mini-Project added

---
+++ app.py
@@ -0,0 +1,61 @@
+# Installing Required libraries
+
+from twilio.rest import Client
+from datetime import datetime, timedelta
+import time
+
+# Twilio account info
+
+account_sid = ""
+auth_token=""
+client = Client(account_sid, auth_token)
+
+
+# message function
+
+def send_message(recipient_number, message):
+    try:
+        message = client.messages.create(
+            from_= "whatsapp:+14155238886",
+            body = message,
+            to = f"whatsapp:{recipient_number}"
+            
+        )
+        print(f"message sent successfully!")
+    except Exception as e:
+        print('an error occurred', e)
+        
+        
+#user input
+name = input('Enter your name : ') 
+recipient_number = input('enter he recipient WhatsApp number with Country code (+91) : ')
+message = input(f"enter the message : ")   
+
+# parse date and time & calculate delay
+
+date_str = input('enter the date to send the message (YYYY-MM-DD) : ')
+time_str = input( "Enter the time to send the message (HH:MM) : ")
+
+# datetime
+
+Schedule_datetime = datetime.strptime(f'{date_str} {time_str}','%Y-%m-%d %H:%M')
+current_datetime = datetime.now()
+
+
+# current delay
+time_difference = Schedule_datetime-current_datetime
+delay_seconds = time_difference.total_seconds()
+
+if delay_seconds <= 0:
+    print('PAST!!!!! Please enter Future Date and Time : ')
+else:
+    print(f"Message scheduled to {name} at {Schedule_datetime} ")
+    
+    # wait until the scheduled time 
+    time.sleep(delay_seconds)
+    
+    
+    #Send the message 
+    send_message(recipient_number, message)
+    
+    
